10a253272728e7b3a2d8706c90961e00ab2db0d2
YaniiiQ <145178403+YaniiiQ@users.noreply.github.com>
2023-10-10 14:50:29 +0800

    Home

--- BSIT3L-Movies/Controllers/HomeController.cs
+++ BSIT3L-Movies/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ public class HomeController : Controller
         _logger = logger;
         _movies = new List<MovieViewModel>
         {
-            new MovieViewModel { Id = 1, Name = "Titanic", Rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama" },
+            new MovieViewModel { Id = 1, Name = "ASDASDASDasd", Rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama" },
             new MovieViewModel { Id = 2, Name = "Inception", Rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller" },
             new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", Rating = "5", ReleaseYear = 1994, Genre = "Drama" },
         };
