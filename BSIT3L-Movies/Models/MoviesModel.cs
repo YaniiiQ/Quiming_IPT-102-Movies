f5e71ec157d0d10654e7dacf79585381bfa2ca51
YaniiiQ <145178403+YaniiiQ@users.noreply.github.com>
2023-10-12 23:34:49 +0800

    Design

--- BSIT3L-Movies/Models/MoviesModel.cs
+++ BSIT3L-Movies/Models/MoviesModel.cs
@@ -9,7 +9,15 @@ namespace BSIT3L_Movies.Models
         public string Rating { get; set; }
         public int ReleaseYear { get; set; }
         public string Genre { get; set; }
-        
+
+        public string Description { get; set; }
+        public string ImageSource { get; set; }
+
+        public string YTurl { get; set; }
+
+        public string movieURL { get; set; }
+
+
     }
 }
 
